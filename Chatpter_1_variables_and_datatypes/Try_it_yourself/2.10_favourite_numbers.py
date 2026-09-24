da2a7434e340a78e0069a5965e3dfee8947e0544
ayush <aayush.multiqos@gmail.com>
2026-09-24 14:16:36 +0530

    Add more Try yourself problems

---
+++ Chatpter_1_variables_and_datatypes/Try_it_yourself/2.10_favourite_numbers.py
@@ -0,0 +1,6 @@
+"""2-10. Favorite Number: Use a variable to represent your favorite number.
+Then, using that variable, create a message that reveals your favorite
+number. Print that message."""
+
+favNumber = 19
+print(f"My favourite number is {favNumber}")
